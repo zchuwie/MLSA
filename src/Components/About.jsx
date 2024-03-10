3100e9d479b82e5fba433da17fe45ca4ae80167c
Denz Christian <mayaloveptr.0108@gmail.com>
2024-03-10 16:28:01 +0800

    .

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../src/images/jon-moore-5fIoyoKlz7A-unsplash.jpg";
+import image from "../images/jon-moore-5fIoyoKlz7A-unsplash.jpg";
 
 const imageAltText = "woodlike background";
 
